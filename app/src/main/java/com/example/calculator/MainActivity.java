2b29f657fff6a363c39531be5bf5cd7f1ad9b737
Algorithmssssssss <reachkim.uni@gmail.com>
2020-09-21 10:32:45 +0300

    This is a test

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.textView = findViewById(R.id.textView);
+    }
+
+    public void changeOnClick(View view) {
+        this.textView.setText("We did it, we change the text!");
     }
 }
